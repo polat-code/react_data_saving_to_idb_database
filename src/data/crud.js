fd9b116b8b95265e3e0d3bcc3ff07965d1995ceb
Özgürhan Polat <ozgurhan.45@gmail.com>
2024-07-30 20:57:18 +0300

    crud is changed

--- src/data/crud.js
+++ src/data/crud.js
@@ -16,3 +16,32 @@ export async function getItems() {
   await tx.done;
   return item;
 }
+
+export async function getItem(id) {
+  const db = await getDb();
+  const tx = db.transaction("store", "readonly");
+  const store = tx.objectStore("store");
+  const item = await store.get(id);
+  await tx.done;
+  return item;
+}
+
+export async function updateItem(id, newData) {
+  const db = await getDb();
+  const tx = db.transaction("store", "readwrite");
+  const store = tx.objectStore("store");
+  const existingItem = await store.get(id);
+  if (existingItem) {
+    const updatedItem = { ...existingItem, ...newData };
+    await store.put(updatedItem);
+  }
+  await tx.done;
+}
+
+export async function deleteItem(id) {
+  const db = await getDb();
+  const tx = db.transaction("store", "readwrite");
+  const store = tx.objectStore("store");
+  await store.delete(id);
+  await tx.done;
+}
